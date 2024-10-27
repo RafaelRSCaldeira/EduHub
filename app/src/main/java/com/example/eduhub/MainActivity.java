d2afadc1f5a2f41b612c6473c9ebf5aed0263a7a
Rafael <rafael.caldeira.2005@gmail.com>
2024-10-27 17:02:22 -0300

    Login page version 1

--- app/src/main/java/com/example/eduhub/MainActivity.java
+++ app/src/main/java/com/example/eduhub/MainActivity.java
@@ -19,8 +19,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        /*binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -31,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.navView, navController);*/
     }
 
 }
