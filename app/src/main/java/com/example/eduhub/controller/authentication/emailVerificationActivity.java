397180de1ab9234065e660444739e0574d8add33
Rafael <rafael.caldeira.2005@gmail.com>
2024-11-03 13:16:48 -0300

    Created email verification page

---
+++ app/src/main/java/com/example/eduhub/controller/authentication/emailVerificationActivity.java
@@ -0,0 +1,33 @@
+package com.example.eduhub.controller.authentication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eduhub.R;
+
+import java.util.Timer;
+
+public class emailVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        Timer timer = new Timer();
+        try {
+            timer.wait(3000);
+            Intent intent = new Intent(emailVerificationActivity.this, SignInActivity.class);
+            startActivity(intent);
+            finish();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
+
