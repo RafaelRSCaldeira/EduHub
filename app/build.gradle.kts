fca85a6d7428d4f132c63cbcf61e82760bd6f92a
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-11-21 00:17:39 -0300

    App version 3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ dependencies {
     implementation(libs.firebase.database)
     implementation(libs.glide)
     implementation(libs.picasso)
-    implementation(libs.rayzone107.durationview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
